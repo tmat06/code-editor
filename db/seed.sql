67cef8064f23cfef99a7b43ae4197a32c31720ec
Mira Jones <meschoolstuffya@gmail.com>
2018-09-10 15:53:02 -0600

    Always messing with the database

--- db/seed.sql
+++ db/seed.sql
@@ -18,7 +18,8 @@ CREATE TABLE tokens (id SERIAL PRIMARY KEY,
                     value VARCHAR,
                     test BOOLEAN,
                     prompt VARCHAR, 
-                    lessonNumber INT)
+                    lessonNumber INT, 
+                    connector TEXT)
 
 
 
@@ -34,65 +35,65 @@ VALUES (1, 1, 'Lesson 1.1', 'We can use the keyword "var" to declare variables, 
 
 INSERT INTO parts
 (number, lessonnumber, title, description, testMode)
-VALUES (2, 1, 'Lesson 1.2', 'Words are represented in memory as a type of data  called "strings". To create a string, we surround a word with quotes. We can also store strings in our variables using the assignment operator (= sign). Store whatever string you want into our greeting variable.', 'Fill in');
+VALUES (2, 1, 'Lesson 1.2', 'Words are represented in memory as a type of data  called "strings". To create a string, we surround a word with quotes. We can also store strings in our variables using the assignment operator (= sign). Store the string "hello" in our greeting variable.');
 
 INSERT INTO parts
 (number, lessonnumber, title, description, testMode)
-VALUES (3, 1, 'Lesson 1.3', '', 'Clickable');
+VALUES (3, 1, 'Lesson 1.3', 'Click on the variable the value "hello" is stored in', 'Clickable');
 
 
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-values (1, 1, 'VarKeyword', 'var', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+values (1, 1, 'VarKeyword', 'var', false, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (2, 1, 'VarName', 'greeting', true, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (2, 1, 'VarName', 'greeting', true, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (3, 1, 'Operator', ';', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (3, 1, 'Operator', ';', false, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES(1, 2, 'VarKeyword', 'var', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES(1, 2, 'VarKeyword', 'var', false, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (2, 2, 'VarName', 'greeting', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (2, 2, 'VarName', 'greeting', false, 'none', 1, "hello");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (3, 2, 'Operator', '=', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (3, 2, 'Operator', '=', false, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (4, 2, 'String', '', true, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (4, 2, 'String', 'hello', true, 'none', 1, "greeting");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (5, 2, 'Operator', ';', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (5, 2, 'Operator', ';', false, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (1, 3, 'VarKeyword', 'var', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (1, 3, 'VarKeyword', 'var', false, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (2, 3, 'VarName', 'greeting', true, 'Click on the variable the value "hello" is stored in', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (2, 3, 'VarName', 'greeting', true, 'none', 1, "hello");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (3, 3, 'Operator', '=', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (3, 3, 'Operator', '=', false, 'none', 1, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (4, 3, 'String', 'hello', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (4, 3, 'String', 'hello', false, 'none', 1, "greeting");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (5, 3, 'Operator', ';', false, 'none', 1);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (5, 3, 'Operator', ';', false, 'none', 1, "undefined");
 
 
 
@@ -112,33 +113,33 @@ VALUES (2, 2, 'Lesson 2.2', 'This is the second part on the second lesson', 'Cli
 
 
 INSERT INTO tokens 
-(number, partnumber, type, value, test, prompt, lessonnumber)
-VALUES(1, 1, 'VarKeyword', 'var', false, 'none', 2)
+(number, partnumber, type, value, test, prompt, lessonnumber, connector)
+VALUES(1, 1, 'VarKeyword', 'var', false, 'none', 2, "undefined")
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (2, 1, 'VarName', 'notGreeting', true, 'none', 2);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (2, 1, 'VarName', 'notGreeting', true, 'none', 2, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (3, 1, 'Operator', ';', false, 'none', 2);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (3, 1, 'Operator', ';', false, 'none', 2, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (1, 2, 'VarKeyword', 'var', false, 'none', 2);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (1, 2, 'VarKeyword', 'var', false, 'none', 2, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (2, 2, 'VarName', 'netGreeting', true, 'This is totally different', 2);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (2, 2, 'VarName', 'netGreeting', true, 'This is totally different', 2, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (3, 2, 'Operator', '=', false, 'none', 2);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (3, 2, 'Operator', '=', false, 'none', 2, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (4, 2, 'String', 'not gonna say hello', false, 'none', 2);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (4, 2, 'String', 'not gonna say hello', false, 'none', 2, "undefined");
 
 INSERT INTO tokens
-(number, partnumber, type, value, test, prompt, lessonNumber)
-VALUES (5, 2, 'Operator', ';', false, 'none', 2);
+(number, partnumber, type, value, test, prompt, lessonNumber, connector)
+VALUES (5, 2, 'Operator', ';', false, 'none', 2, "undefined");
--- src/components/LessonPartsList.js
+++ src/components/LessonPartsList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { updateLesson } from "./../ducks/reducer";
 import { connect } from "net";
+import axios from "axios";
 
 class LessonPartsList extends React.Component {
   constructor() {
@@ -10,8 +11,10 @@ class LessonPartsList extends React.Component {
 
   componentDidMount() {
     const { lesson } = this.props.match.params;
+    axios.get(`/api/lesson/${lesson}`).then(lessonInfo => {
+      this.props.updateLesson(lessonInfo);
+    })
     //call to the database using this.props.match.params.lesson to indicate which lesson needs to be pushed up to redux
-    this.props.updateLesson(lesson);
   }
   render() {
     return <div>Dog</div>;
