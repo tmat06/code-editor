2fd041e86b15cc89eb67060444fdc4aff5f66693
Mira Jones <meschoolstuffya@gmail.com>
2018-09-13 17:11:22 -0600

    Removed code I don't need

--- server/controller.js
+++ server/controller.js
@@ -88,25 +88,5 @@ module.exports = {
         }
         allTheQuizTokens();
       });
-
-    // var goodLessonLayout = []
-    // req.app.get("db").getLesson(id).then(lesson => {
-    //     req.app.get("db").getParts(id).then(parts => {
-    //         async function crazyStuff(){
-    //             for(var i = 0; i<parts.length; i++){
-    //                 await req.app.get("db").getTokens(i + 1, id).then(tokens => {
-    //                     goodLessonLayout.push({
-    //                         title: parts[i].title,
-    //                         description: parts[i].description,
-    //                         testMode: parts[i].testmode,
-    //                         tokens: tokens
-    //                     })
-    //                 })
-    //             }
-    //             res.status(200).send(goodLessonLayout)
-    //         }
-    //         crazyStuff()
-    //     })
-    // })
   }
 };
